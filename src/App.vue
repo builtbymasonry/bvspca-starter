e61b002b501f1e7d37e8e1247a87ded92cfbcb3b
Christopher Kennedy-Nuñez <chris@onebrightlight.com>
2022-06-23 16:15:07 -0700

    ✨ add new component examples

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+  import { ref } from 'vue'
+  import Header from './components/Header.vue'
 
-let counter = ref(0)
+  let counter = ref(0)
 
-setInterval(() => {
-  counter.value++
-}, 1000)
+  setInterval(() => {
+    counter.value++
+  }, 1000)
 </script>
 
 <template>
+  <Header />
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <h1
           @click="counter = 0"
           class="text-3xl font-bold leading-tight text-gray-900"
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    target="{{ target }}"
+    href="{{ url }}"
+    class="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700"
+    >{{ text }}</a
+  >
+</template>
+
+<script>
+  export default {
+    props: {
+      target: {
+        type: String,
+        default: '_self'
+      },
+      url: {
+        type: String,
+        default: '#'
+      },
+      text: {
+        type: String,
+        default: 'Button'
+      }
+    }
+  }
+</script>
