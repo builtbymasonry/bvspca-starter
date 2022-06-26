8569796d232bed2f55c1a1c861cbc41dc7f4adc0
yusupov_i <info@heyreliable.com>
2022-06-26 19:13:13 +0300

    clean template

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <script setup>
-  import { ref } from 'vue'
-  import Header from './components/Header.vue'
+import { ref } from "vue";
+import Header from "./components/ExampleHeader.vue";
 
-  let counter = ref(0)
-
-  setInterval(() => {
-    counter.value++
-  }, 1000)
+let counter = ref(0);
 </script>
 
 <template>
   <Header />
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1
           @click="counter = 0"
           class="text-3xl font-bold leading-tight text-gray-900"
         >
-          {{ $route.meta.title }} / {{ counter }}
+          {{ $route.meta.title }}
         </h1>
       </div>
     </header>
