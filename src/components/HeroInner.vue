69c71003fb60594ff3b8b5097169c99696ccff92
sivukha.s <info@heyreliable.com>
2022-07-13 16:29:49 +0300

    wellness-clinic page progress

--- src/components/HeroInner.vue
+++ src/components/HeroInner.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="relative overflow-hidden">
-    <img
-      class="relative top-0 left-0 w-full object-cover md:absolute md:h-full"
-      :src="imgSrc"
-      :alt="imgDescr"
-    />
+    <div class="mx-auto max-w-7xl px-4">
+      <img
+        class="relative top-0 left-0 w-full object-cover md:absolute md:h-full"
+        :src="imgSrc"
+        :alt="imgDescr"
+      />
 
-    <div class="relative md:bg-gradient-radial from-white to-white/30 md:max-w-lg px-5 md:px-12 xl:px-20 pb-14 py-8 md:py-12 xl:py-20 md:mx-10 xl:mx-20 2xl:mx-40 md:my-12 lg:my-20 md:rounded-lg text-center md:text-left">
-      <h1 class="mb-2 font-bold text-4xl md:text-5xl lg:text-6xl">
-      For your pet
-      </h1>
-      <p class="text-base lg:text-lg">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in
-      dapibus nisl.
-      </p>
+      <div class="relative bg-gray-dark md:bg-transparent md:bg-gradient-radial from-white to-white/30 md:max-w-lg px-5 md:px-12 xl:px-20 pb-10 py-6 md:py-12 xl:py-20 md:my-12 lg:my-20 md:rounded-lg text-center md:text-left">
+        <h1 class="mb-2 font-bold text-4xl md:text-5xl lg:text-6xl">{{title}}</h1>
+        <p class="text-sm md:text-base lg:text-lg">{{text}}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -30,5 +27,7 @@ const props = defineProps({
     type: String,
     default: "Image Description",
   },
+  title: String,
+  text: String
 });
 </script>
